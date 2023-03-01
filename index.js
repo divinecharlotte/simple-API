46bf5e05f1396a5acc8a4b6555f3db0bc5eb20df
allyleandre <tuyambazeleandre@gmail.com>
2023-03-01 16:35:47 +0200

    improve project setup

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const db = require("./models");
 
-const db = require("./models")
-const { User } = require('./models')
-app.get('/select',(req,res) => {
-    res.send("select")
-})
+const userRoutes = require("./routes/users");
 
-app.get('/insert',(req,res) => {
+app.use("/users", userRoutes);
 
-    User.create({
-        firstName: "divine",
-        email: "divinemaina@gmail.com"
-    }).catch(err =>{
-        if (err){
-            console.log(err)
-        }
-    })
-    res.send("insert")
-})
-
-db.sequelize.sync().then((res)=>{
-
-    app.listen(3000, ()=>{
-        console.log("server started")
-    })
-})
+db.sequelize.sync().then((res) => {
+  app.listen(3000, () => {
+    console.log("server started");
+  });
+});
